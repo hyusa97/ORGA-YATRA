cb66a6ab8548263029bbaf6d7880459486f03bc9
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2025-08-06 14:05:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -319,7 +319,7 @@ else:
         # === Combined Totals ===
         total_collection = govind_total_collection + gaurav_total_collection
         total_investment = govind_total_investment + gaurav_total_investment + Investment_Credit_Bank
-        total_expense = govind_total_expense + gaurav_total_expense
+        total_expense = govind_total_expense + gaurav_total_expense + govind_expense_debit +gaurav_expense_debit
 
         remaining_fund_gaurav= (gaurav_total_collection - gaurav_total_expense - gaurav_collection_credit + gaurav_settlement_debit - gaurav_settlement_credit + gaurav_total_investment)
         remaining_fund_govind= (govind_total_collection - govind_total_expense - govind_collection_credit + govind_settlement_debit - govind_settlement_credit + govind_total_investment)
