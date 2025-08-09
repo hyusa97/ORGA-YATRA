c3faba489be566d9bbf6799e5facbf051a68a083
hyusa09 <159457146+hyusa09@users.noreply.github.com>
2025-08-09 10:23:08 +0530

    yes

--- main.py
+++ main.py
@@ -446,10 +446,10 @@ else:
 
         # Raise Collection Button
         google_form_url = "https://docs.google.com/forms/d/e/1FAIpQLSdnNBpKKxpWVkrZfj0PLKW8K26-3i0bO43hBADOHvGcpGqjvA/viewform?usp=header"
-        col1, col2, col3 = st.columns([8, 1, 1])
+        col1, col2, col3 = st.columns([10, 2, 1])
         with col2:
             if st.button("Add Collection ➕", type="primary"):
-                st.markdown(f"[Opening Google Form...]({google_form_url})", unsafe_allow_html=True)
+                st.markdown(f"[Open Google Form]({google_form_url})", unsafe_allow_html=True)
                 #st.experimental_set_query_params(open_form="1")
                 #st.write(f"[Click here to open the form]({google_form_url})")
 
