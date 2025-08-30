144a8af205c5a73c6cce887313e4c2abb3cadbcf
hyusa09 <159457146+hyusa09@users.noreply.github.com>
2025-08-31 00:08:30 +0530

    ss

--- main.py
+++ main.py
@@ -1478,7 +1478,7 @@ else:
         
 
         st.markdown("---")
-        col0, col2 = st.columns(3)
+        col0, col1, col2 = st.columns(3)
         col0.metric("Filtered Driver Loss", f"{f_driver_loss:,.0f}")
         col1.metric(" ")
         col2.metric("Filtered Company Loss", f"{f_company_loss:,.0f}")
